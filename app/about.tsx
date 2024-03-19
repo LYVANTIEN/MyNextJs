87c1faa2daf441e9bfd8309b3b1fb46e6bc2fa0c
Tienvan2112 <55881057+LYVANTIEN@users.noreply.github.com>
2024-03-19 20:46:34 +0700

    new

---
+++ app/about.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface aboutProps {
+}
+
+export default function AboutPahe (props: aboutProps) {
+  return (
+    <div>
+      hello new 
+    </div>
+  );
+}
